4d9c2475bb8ddedc536f18c3dbb10b5084ccbf87
arthuroliveiraa <arthur.oliveira@cnecuberaba.edu.br>
2020-06-10 10:21:41 -0300

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>About Jokes</h1>
+      <p>This is a app that displays corny jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: 'About The App',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for corny jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Welcome to the best jokes app on the WEB</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  head(){
+        return {
+            title: 'Welcome to Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for corny jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
